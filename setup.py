6afa7c4f1e19338d12d31bc0e9ea411b6e4dbac0
andreffs18 <andreffs18@gmail.com>
2017-08-30 14:06:45 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sheetsu',
-    version='0.0.3',
+    version='0.0.4',
     description='Sheetsu Python client',
     url='http://github.com/andreffs18/sheetsu-python',
     author='Andre Silva',
