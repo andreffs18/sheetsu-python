70189c54cfbe07b819fcd23fbe213be9de5b4db2
andreffs18 <andreffs18@gmail.com>
2017-08-30 14:14:55 +0100

    Add required packages for lib to work and update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sheetsu',
-    version='0.0.4',
+    version='0.0.5',
     description='Sheetsu Python client',
     url='http://github.com/andreffs18/sheetsu-python',
     author='Andre Silva',
@@ -10,5 +10,6 @@ setup(
     license='MIT',
     keywords='sheetsu api client sdk spreadsheet',
     packages=find_packages(),
-    zip_safe=False
+    zip_safe=False,
+    install_requires=['requests']
 )
